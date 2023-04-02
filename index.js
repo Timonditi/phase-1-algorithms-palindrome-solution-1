493fd800f3f487e23d22dd5c617a45f5f95f6d2b
Timothy <todongoonditi@gmail.com>
2023-04-02 13:32:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
 }
 
+console.assert(isPalindrome('abba') === true, 'Expected isPalindrome("abba") to be true');
+
 /* 
-  Add your pseudocode here
+// Compare the first and last characters of the string,
+// then move inward until the whole string has been checked
+// If we've made it through the loop without returning false,
+// the string is a palindrome
+// Test case
 */
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
